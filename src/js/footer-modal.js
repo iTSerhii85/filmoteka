df8f23f0d8ed55f82460465eed3732114695a8b6
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2023-01-26 20:22:28 +0200

    modal stiles

--- src/js/footer-modal.js
+++ src/js/footer-modal.js
@@ -8,9 +8,11 @@ openModal.addEventListener('click', openModalWindow);
 const developerMarkup = developers.map(obj =>`
 <div class="developer">
   <div class="photo">${obj.img}</div>
-    <div class="about">
-      <h4 class="dev-name">${obj.name}</h4>
-      <p class="about-dev">${obj.text}</p>
+    <div class="border">
+      <div class="about">
+        <h4 class="dev-name">${obj.name}</h4>
+        <p class="about-dev">${obj.text}</p>
+      </div>
     </div>
   </div>`).join('');
 
@@ -18,9 +20,7 @@ const markup = `
 <div class="footer-modal-backdrop">
   <div class="footer-modal">
     <button type="button" class="footer-modal-close-button">
-      <svg class="modal-close" width="18" height="18">
-        <use class="icon-x" href="./images/svg/sprite.svg#icon-svg-heart"></use>
-      </svg>
+    X
     </button>
     <h2 class="our-team">Our team</h2>
    ${developerMarkup}
