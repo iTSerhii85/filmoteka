91d219143748923fd994cd535bdca0d5e40a8c6b
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2023-01-26 02:31:32 +0200

    Created markup inside the modal

---
+++ src/js/developers.js
@@ -0,0 +1,32 @@
+export const developers = [
+    {
+      name: 'Serphii Prokip',
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      img: ''
+    },
+    {
+      name: 'Yulia Safonova',
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      img: ''
+    },
+    {
+      name: 'Roman Korzun',
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      img: ''
+    },
+    {
+      name: 'Taras Bohun',
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      img: ''
+    },
+    {
+      name: 'Svitlana',
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      img: ''
+    },
+    {
+      name: 'Evgeniy',
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      img: ''
+    }
+  ];
--- src/js/footer-modal.js
+++ src/js/footer-modal.js
@@ -1,13 +1,29 @@
+import { developers } from "./developers";
+
 const modal = document.querySelector('.js-footer-modal');
 const openModal = document.querySelector('.footer-open-modal_link');
 
 openModal.addEventListener('click', openModalWindow);
 
+const developerMarkup = developers.map(obj =>`
+<div class="developer">
+  <div class="photo">${obj.img}</div>
+    <div class="about">
+      <h4 class="dev-name">${obj.name}</h4>
+      <p class="about-dev">${obj.text}</p>
+    </div>
+  </div>`).join('');
+
 const markup = `
 <div class="footer-modal-backdrop">
   <div class="footer-modal">
     <button type="button" class="footer-modal-close-button">
+      <svg class="modal-close" width="18" height="18">
+        <use class="icon-x" href="./images/svg/sprite.svg#icon-svg-heart"></use>
+      </svg>
     </button>
+    <h2 class="our-team">Our team</h2>
+   ${developerMarkup}
   </div>
 </div>`;
 
@@ -20,3 +36,4 @@ function openModalWindow(){
 function closeModalWindow(){
     modal.innerHTML = ``;
 }
+
