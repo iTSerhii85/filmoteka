0283855ab085089b1f5c1a7019ee42a86946d9a4
iTSerhii85 <109918837+iTSerhii85@users.noreply.github.com>
2023-01-26 17:11:28 +0000

    Deploying to gh-pages from @ iTSerhii85/filmoteka@875b91a6cae0cb1de0e546d178fbb7df65cb8b14 🚀

--- index.fda1185c.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=document.querySelector(".js-footer-modal");document.querySelector(".footer-open-modal_link").addEventListener("click",(function(){o.innerHTML='\n<div class="footer-modal-backdrop">\n  <div class="footer-modal">\n    <button type="button" class="footer-modal-close-button">\n    </button>\n  </div>\n</div>',document.querySelector(".footer-modal-close-button").addEventListener("click",e)}));function e(){o.innerHTML=""}}();
-//# sourceMappingURL=index.fda1185c.js.map
