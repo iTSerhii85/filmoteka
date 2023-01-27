e910fe94b86fa310e84febad2dac41247e6684b3
Dmytro1001 <31dmitri13@gmail.com>
2023-01-27 22:16:30 +0200

    add

---
+++ src/js/modalMovieWindow.js
@@ -0,0 +1,41 @@
+const cardList = document.querySelector('.cards__list');
+const backdrop = document.querySelector('.backdrop-modal');
+const closeModalBtn = document.querySelector('.button-close');
+const movieCard = document.querySelector('.modal-movie-card');
+
+cardList.addEventListener('click', openModal);
+
+function openModal(event) {
+  closeModalBtn.addEventListener('click', closeModal);
+  backdrop.addEventListener('click', event => closeModalBackdrop(event));
+  document.addEventListener('keydown', event => closeModalEsc(event));
+  backdrop.classList.remove('is-hidden');
+
+  setTimeout(() => {
+    backdrop.firstElementChild.classList.remove('is-hidden');
+  }, 350);
+  document.body.classList.add('modal-open');
+}
+
+function closeModal(event) {
+  backdrop.classList.add('is-hidden');
+  backdrop.firstElementChild.classList.remove('is-hidden');
+  closeModalBtn.removeEventListener('click', closeModal);
+  backdrop.removeEventListener('click', closeModal);
+  document.removeEventListener('keydown', event => closeModalEsc(event));
+  document.body.classList.remove('modal-open');
+}
+
+function closeModalBackdrop(event) {
+  if (event.target.classList.value !== 'backdrop-modal') {
+    return;
+  }
+  closeModal();
+}
+
+function closeModalEsc(event) {
+  if (event.key !== 'Escape') {
+    return;
+  }
+  closeModal();
+}
