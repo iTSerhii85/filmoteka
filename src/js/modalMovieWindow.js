88d4e2ebaec3fa635a71eded16ab58f234788ba7
Dmytro1001 <31dmitri13@gmail.com>
2023-01-28 14:50:28 +0200

    Модальне вікно
    Болюча тема та розмітка хто може допомогти її прикрутити.

--- src/js/modalMovieWindow.js
+++ src/js/modalMovieWindow.js
@@ -9,7 +9,6 @@ const START_URL = 'https://image.tmdb.org/t/p/w500'
 cardList.addEventListener('click', event => {
     if (event.target.nodeName !== 'BUTTON') {
         renderModal(event);
-        console.log('hello')
     }
 });
 
@@ -56,48 +55,119 @@ const renderModal = event => {
     }
     const cardId = event.target.closest('li');
     const idMovie = cardId.id;
-    const data = getFilmDetails(idMovie) //----- Невиходить повернути об'єкти з відповідним id.
-    console.log(data)
-
+    const savedTrendingFilms = localStorage.getItem('TRENDING_DATA_KEY');
+    const arrayMovies = JSON.parse(savedTrendingFilms);
+    const data = arrayMovies.find(arr => arr.id === Number(idMovie));
     
     
     if (data) {
         currentId = data.id;
-        // renderMovieCard(data);
+        renderMovieCard(data);
         openModal(event);
     }
     
 };
 
 
-function checkGenresById(obj) {
-  const savedGenresData = localStorage.getItem('GENRES_DATA_KEY')
-  const parseGenresData = JSON.parse(savedGenresData)
-  let genresArr = [];
+function renderMovieCard(data) {
 
-    const genresIds = obj.genre_ids
+  // checkGenresById(data)
+     
+  // const movie = data.map(obj => `<div class='modal-movie-card__wrappe-img'><img class='modal-movie-card__image' src='${START_URL}${{poster_path}}' alt='${{title}}'
+  //                   loading='lazy' />
+  //           </div>
+  //           <div class='modal-movie-data'>
+  //               <h2 class='modal-movie-data__title'>${{title}}</h2>
 
-    for (const parseGenre of parseGenresData) {
-      
-      if (genresIds.includes(parseGenre.id)) {
-        genresArr.push(parseGenre.name)
-      }
-      
-    }
-    // console.log(genresArr)  
-  
-  const genresStr = genresArr.join(', ');
-  return genresStr;
-}
+  //               <table class='modal-movie-data-table'>
+  //                   <tr class='modal-movie-data-table__row'>
+  //                       <td>
+  //                           <p class='modal-movie-data__attribute'>Vote / Votes</p>
+  //                       </td>
+  //                       <td>
+  //                           <p><span class='modal-movie-data__vote'>${{vote}}</span> / ${{votes}}</p>
+  //                       </td>
 
-function getFilmDetails(idMovie) {
-  const savedTrendingFilms = localStorage.getItem('TRENDING_DATA_KEY');
-  const arrayMovies = JSON.parse(savedTrendingFilms);
-  for (const arr of arrayMovies) {
-    if (arr.id === idMovie) {
-      return arr;
-      break
-    }   
-    console.log(arr)
+  //                   </tr>
+  //                   <tr class='modal-movie-data-table__row'>
+  //                       <td>
+  //                           <p>Popularity</p>
+  //                       </td>
+  //                       <td>
+  //                           <p>${{popularity}}</p>
+  //                       </td>
+
+  //                   </tr>
+  //                   <tr class='modal-movie-data-table__row'>
+  //                       <td>
+  //                           <p>Original Title</p>
+  //                       </td>
+  //                       <td>
+  //                           <p>${{original_title}}</p>
+  //                       </td>
+
+  //                   </tr>
+  //                   <tr class='modal-movie-data-table__row'>
+  //                       <td>
+  //                           <p>Genre</p>
+  //                       </td>
+  //                       <td>
+  //                           // <p>{checkGenresById(data)}</p>
+  //                       </td>
+
+  //                   </tr>
+  //               </table>
+  //               <h3 class='modal-movie-data__about_title'>About</h3>
+  //               <p class='modal-movie-data__about'>
+  //                   ${{overview}}
+  //               </p>
+
+
+  //               <div class='modal-movie-btn-wrapper' style='clear: both'>
+  //                   <button class='button-modal btn-to-watched' type='button' data-action='${{id}}'>
+  //                       ADD TO WATCHED
+  //                   </button>
+  //                   <button class='button-modal btn-to-queue' type='button' data-action='${{id}}'>
+  //                       ADD TO QUEUE
+  //                   </button>
+  //                   <button class='button-modal btn-to-watched' type='button' data-action='${{id}}'>
+  //                       REMOVE FROM WATCHED
+  //                   </button>
+  //                   <button class='button-modal btn-to-queue' type='button' data-action='${{id}}'>
+  //                       REMOVE FROM QUEUE
+  //                   </button>
+  //               </div>
+  //           </div>`).join('');
+
+  // movieCard.innerHTML(movie);
+
+    const backdropImage = data.backdrop_path;
+    if (backdropImage !== null) {
+    const background = `https://image.tmdb.org/t/p/original/${data.backdrop_path}`;
+    backdrop.style.backgroundImage = `url('${background}')`;
+    backdrop.style.backgroundSize = 'cover';
+    backdrop.style.backgroundPosition = '50% 50%';
   }
 }
+
+// const movieInfo 
+
+// function checkGenresById(obj) {
+//   const savedGenresData = localStorage.getItem('GENRES_DATA_KEY')
+//   const parseGenresData = JSON.parse(savedGenresData)
+//   let genresArr = [];
+
+//     const genresIds = obj.genre_ids
+
+//     for (const parseGenre of parseGenresData) {
+      
+//       if (genresIds.includes(parseGenre.id)) {
+//         genresArr.push(parseGenre.name)
+//       }
+      
+//     }
+//     // console.log(genresArr)  
+  
+//   const genresStr = genresArr.join(', ');
+//   return genresStr;
+// }
