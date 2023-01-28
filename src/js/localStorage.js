b511f770929c545b00b96c49939c28a555291343
Yulia-Safonova-85 <109626421+Yulia-Safonova-85@users.noreply.github.com>
2023-01-28 14:29:35 +0200

    add func Empty

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,3 +1,42 @@
+const refs = {
+    formMessage: document.querySelector('.header-form__message'),
+    searchBtn: document.querySelector('.search-btn'),
+    input: document.querySelector('.header-form__input')
+}
+    
+refs.input.addEventListener('input', onInputEmpty)
+
+function onInputEmpty(evt) {
+    const inputValue = refs.input.value;
+
+    if (inputValue) {
+        refs.searchBtn.style.display = 'block';
+
+        refs.searchBtn.addEventListener('click', () => {
+            refs.searchBtn.style.display = 'none';
+            refs.input.value = '';
+            return
+        } )
+    }
+}
+//  функція яка викликається якщо нічого не введено або нема результату
+function emptyNoResultInput() {
+    refs.formMessage.insertAdjacentHTML('beforeend', '<p class="header-form__message">Search result not successfull.Enter the correct movie name</p>');
+    setTimeout(() => {
+        refs.formMessage.innerHTML = '';
+    }, 3000);
+
+    getTrendingMovies().then(data => {
+  createMarkup(data.results)
+  
+})
+}
+
+
+
+
+
+
 const save = (key, value) => {
     
     try {
