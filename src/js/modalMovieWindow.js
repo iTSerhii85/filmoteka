c378a05203b1b51678f8b86208f25f8963768e8f
Dmytro1001 <31dmitri13@gmail.com>
2023-01-28 07:54:42 +0200

    пушу щоб непропала
    Якщо можеш подивись , застряг на 59строці там все написано

--- src/js/modalMovieWindow.js
+++ src/js/modalMovieWindow.js
@@ -1,9 +1,19 @@
+let currentId = null;
 const cardList = document.querySelector('.js-main-gallery');
 const backdrop = document.querySelector('.backdrop-modal');
 const closeModalBtn = document.querySelector('.button-close');
 const movieCard = document.querySelector('.modal-movie-card');
 
-cardList.addEventListener('click', openModal);
+const START_URL = 'https://image.tmdb.org/t/p/w500'
+
+cardList.addEventListener('click', event => {
+    if (event.target.nodeName !== 'BUTTON') {
+        renderModal(event);
+        console.log('hello')
+    }
+});
+
+// cardList.addEventListener('click', openModal);
 
 function openModal(event) {
   closeModalBtn.addEventListener('click', closeModal);
@@ -38,4 +48,56 @@ function closeModalEsc(event) {
     return;
   }
   closeModal();
+}
+
+const renderModal = event => {
+    if (event.target.nodeName === 'BUTTON') {
+        return
+    }
+    const cardId = event.target.closest('li');
+    const idMovie = cardId.id;
+    const data = getFilmDetails(idMovie) //----- Невиходить повернути об'єкти з відповідним id.
+    console.log(data)
+
+    
+    
+    if (data) {
+        currentId = data.id;
+        // renderMovieCard(data);
+        openModal(event);
+    }
+    
+};
+
+
+function checkGenresById(obj) {
+  const savedGenresData = localStorage.getItem('GENRES_DATA_KEY')
+  const parseGenresData = JSON.parse(savedGenresData)
+  let genresArr = [];
+
+    const genresIds = obj.genre_ids
+
+    for (const parseGenre of parseGenresData) {
+      
+      if (genresIds.includes(parseGenre.id)) {
+        genresArr.push(parseGenre.name)
+      }
+      
+    }
+    // console.log(genresArr)  
+  
+  const genresStr = genresArr.join(', ');
+  return genresStr;
+}
+
+function getFilmDetails(idMovie) {
+  const savedTrendingFilms = localStorage.getItem('TRENDING_DATA_KEY');
+  const arrayMovies = JSON.parse(savedTrendingFilms);
+  for (const arr of arrayMovies) {
+    if (arr.id === idMovie) {
+      return arr;
+      break
+    }   
+    console.log(arr)
+  }
 }
