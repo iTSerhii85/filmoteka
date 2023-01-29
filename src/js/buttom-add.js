7af084e348bb1bae65daa8f1303cd7bca4a367b5
Svitlana Shvydanenko <Svik_Svik@ukr.net>
2023-01-29 19:18:05 +0200

    для отримання нових оновлень

--- src/js/buttom-add.js
+++ src/js/buttom-add.js
@@ -11,35 +11,99 @@ btmToQueue.addEventListener('click', onToQueueBtn);
 removeWatchedBtn.addEventListener('click', onRemoveWatchedBtn);
 removeQueueBtn.addEventListener('click', onRemoveQueueBtn);
 
-const catchModal = document.querySelector('.modal-movie-card');
-catchModal.addEventListener('show', catchModalData);
+// const catchModal = document.querySelector('.card-wraper');
+// catchModal.addEventListener('show', catchModalData);
 
-let watchedElId = watched.getElementById();
-let queueElId = queue.getElementById();
+let watchedEl = watched.getElementById();
+let queueEl = queue.getElementById();
 
-function catchModalData() {
-let movieId = Object.getElementById();
+// function catchModalData() {
+// let movieId = Object.getElementById();
     
-    if (movieId === null) {
-        toWatchedBtn.disabled = true;
-        toQueueBtn.disabled = true;
-        removeWatchedBtn.disabled = true;
-        removeQueueBtn.disabled = true;
-        return;
-    }
-    if (movieId !== watchedElId) {
-      toWatchedBtn.disabled = false;
-    }
+//     if (movieId === null) {
+//         toWatchedBtn.disabled = true;
+//         toQueueBtn.disabled = true;
+//         removeWatchedBtn.disabled = true;
+//         removeQueueBtn.disabled = true;
+//         return;
+//     }
+//     if (movieId !== watchedEl) {
+//       toWatchedBtn.disabled = false;
+//     }
 
-    if (movieId === watchedElId) {
-        removeWatchedBtn.disabled = false;
+//     if (movieId === watchedEl) {
+//         removeWatchedBtn.disabled = false;
+//     }
+
+//      if (movieId !== queueEl) {
+//        toQueueBtn.disabled = false;
+//      }
+
+//      if (movieId === queueEl) {
+//          removeQueueBtn.disabled = false;
+//      }
+// }
+
+function onToWatchedBtn() {
+    let alreadyExists = watched.find((watched) => watched.id === watchedEl.id);
+    
+    if (typeof alreadyExists === "undefined") {
+      watched.push(watchedEl);
+      // watched.unshift(watchedEl);
+      console.log(`${watchedEl} added to watched`);
+
+      toWatchedBtn.disabled = true;
+      removeWatchedBtn.disabled = false;
+      // toWatchedBtn.hide;
+      // removeWatchedBtn.show;
+       return;
     }
+  return;
+}
 
-     if (movieId !== queueElId) {
-       toQueueBtn.disabled = false;
-     }
+function onRemoveWatchedBtn() {
+  try {
+    alreadyExists === "fined";
+    watched.remove(watchedEl);
+    removeWatchedBtn.hide;
+    toWatchedBtn.show;
 
-     if (movieId === queueElId) {
-         removeQueueBtn.disabled = false;
-     }
+    return;
+  }
+  
+    catch (error) {
+    console.log(error);
+    alert ("Sorry, try again later");
+    }
+}
+
+function onToQueueBtn() {
+    let alreadyQueue = queue.find(queue => queue.id === queueEl.id);
+
+    if (typeof alreadyQueue === 'undefined') {
+      queue.push(queueEl);
+      console.log(`${queueEl} added to queue`);
+      btmToQueue.hide;
+      removeQueueBtn.show;
+      return;
+    
+  } 
+  return;
+}
+
+function onRemoveQueueBtn() {
+try {
+  alreadyQueue === "fined";
+  queue.remove(queueEl);
+  
+  removeQueueBtn.hide;
+  btmToQueue.show;
+
+    return;
+  }
+  
+    catch (error) {
+    console.log(error);
+    alert ("Sorry, try again later");
+    }
 }
