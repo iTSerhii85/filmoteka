82052a29830b9b73fe618c28f73b100f4be7c55b
Svitlana Shvydanenko <Svik_Svik@ukr.net>
2023-01-30 12:43:52 +0200

    Налаштування роботи btn-add

--- src/js/buttom-add.js
+++ src/js/buttom-add.js
@@ -1,13 +1,23 @@
 let watched = new Array();
 
 function saveWatchedListToLocalStorage(data) {
-  localStorage.setItem('WATCHED_LIST_DATA_KEY', JSON.stringify(data.results));
+  localStorage.setItem('WATCHED_LIST_DATA_KEY', JSON.stringify(data));
 }
 
 let queue = new Array();
 
 function saveQueueListToLocalStorage(data) {
-  localStorage.setItem('Queue_LIST_DATA_KEY', JSON.stringify(data.results));
+  localStorage.setItem('QUEUE_LIST_DATA_KEY', JSON.stringify(data));
+}
+
+let watchedListFromLocalStorage = localStorage.getItem('WATCHED_LIST_DATA_KEY');
+if (watchedListFromLocalStorage != null) {
+  watched = JSON.parse(watchedListFromLocalStorage);
+}
+
+let queueListFromLocalStorage = localStorage.getItem('QUEUE_LIST_DATA_KEY');
+if (queueListFromLocalStorage != null) {
+  queue = JSON.parse(queueListFromLocalStorage);
 }
 
 const toWatchedBtn = document.querySelector('.js-btn-to-watched');
@@ -29,77 +39,79 @@ function onToWatchedBtn(event) {
   const idMovie = button.id;
   console.log(idMovie);
   console.log('onToWatchedBt entered');
-  let alreadyExists = watched.find(watched => watched.id === idMovie);
+  let alreadyExists = watched.find(element => element === idMovie);
 
   if (typeof alreadyExists === 'undefined') {
     watched.push(idMovie);
+    saveWatchedListToLocalStorage(watched);
     console.log(`${idMovie} added to watched`);
 
-    ('toWatchedBtn');
-    ('removeWatchedBtn');
+    // 'toWatchedBtn';
+    // 'removeWatchedBtn';
 
     return;
   }
   return;
 }
 
-function onRemoveWatchedBtn(event) {
-  const button = event.currentTarget;
+// function onRemoveWatchedBtn(event) {
+//   const button = event.currentTarget;
 
-  const idMovie = button.id;
-  console.log(idMovie);
-  console.log('onRemoveWatchedBtn entered');
-  let alreadyExists = watched.find(watched => watched.id === idMovie);
+//   const idMovie = button.id;
+//   console.log(idMovie);
+//   console.log('onRemoveWatchedBtn entered');
+//   let alreadyExists = watched.find(element => element === idMovie);
 
-  if (typeof alreadyExists === 'fined') {
-    watched.remove(idMovie);
-    console.log(`${idMovie} remove from watched`);
+//   if (typeof alreadyExists === 'fined') {
+//     watched.remove(idMovie);
+//     console.log(`${idMovie} remove from watched`);
 
-    ('toWatchedBtn');
-    ('removeWatchedBtn');
+//     ('toWatchedBtn');
+//     ('removeWatchedBtn');
 
-    return;
-  }
-  return;
-}
+//     return;
+//   }
+//   return;
+// }
 
-function onToQueueBtn(event) {
-  const button = event.currentTarget;
+// function onToQueueBtn(event) {
+//   const button = event.currentTarget;
 
-  const idMovie = button.id;
-  console.log(idMovie);
-  console.log('onToQueueBtn entered');
+//   console.log(button);
+//   const idMovie = button.id;
+//   console.log(idMovie);
+//   console.log('onToQueueBtn entered');
+//   let alreadyQueue = queue.find(element => element === idMovie);
 
-  let alreadyQueue = queue.find(queue => queue.id === idMovie);
+//   if (typeof alreadyQueue === 'undefined') {
+//     queue.push(idMovie);
+//     saveQueueListToLocalStorage(queue);
+//     console.log(`${idMovie} added to queue`);
 
-  if (typeof alreadyQueue === 'undefined') {
-    queue.push(idMovie);
-    console.log(`${idMovie} added to queue`);
+//     // ('toQueueBtn');
+//     // ('removeQueueBtn');
 
-    ('toQueueBtn');
-    ('removeQueueBtn');
+//     return;
+//   }
+//   return;
+// }
 
-    return;
-  }
-  return;
-}
+// function onRemoveQueueBtn(event) {
+//   const button = event.currentTarget;
 
-function onRemoveQueueBtn(event) {
-  const button = event.currentTarget;
+//   const idMovie = button.id;
+//   console.log(idMovie);
+//   console.log('onRemoveQueueBtn entered');
+//   let alreadyQueue = queue.find(element => element === idMovie);
 
-  const idMovie = button.id;
-  console.log(idMovie);
-  console.log('onRemoveQueueBtn entered');
-  let alreadyQueue = queue.find(queue => queue.id === idMovie);
+//   if (typeof alreadyQueue === 'fined') {
+//     queue.remove(idMovie);
+//     console.log(`${idMovie} remove from queue`);
 
-  if (typeof alreadyQueue === 'fined') {
-    queue.remove(idMovie);
-    console.log(`${idMovie} remove from queue`);
+//     ('toQueueBtn');
+//     ('removeQueueBtn');
 
-    ('toQueueBtn');
-    ('removeQueueBtn');
-
-    return;
-  }
-  return;
-}
+//     return;
+//   }
+//   return;
+// }
