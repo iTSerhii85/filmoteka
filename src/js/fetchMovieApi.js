a98c456b46f5a53301e56ba514356dd5e3fdfc53
DWIJIx <106489060+DWIJIx@users.noreply.github.com>
2023-01-30 21:03:08 +0200

    cleaned code

--- src/js/fetchMovieApi.js
+++ src/js/fetchMovieApi.js
@@ -2,7 +2,7 @@ const axios = require('axios').default;
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const KEY = '7c6a11c2d8e8524ccfd41d8aedf2bd73';
 
-// створюємо клас MovieApiService з методом getMovies()
+// створюємо клас MovieApiService
 export default class MovieApiService {
     constructor() {
         this.inputValue = '';
@@ -12,9 +12,6 @@ export default class MovieApiService {
     async getGenres() {
         try {
             const response = await axios.get(`${BASE_URL}genre/movie/list?api_key=${KEY}`);
-            // console.log(response.data);
-            // Збільшуємо значення this.page += 1
-            // this.incrementPage();
             return response.data;
         } catch (error) {
             console.error(error);
@@ -24,29 +21,23 @@ export default class MovieApiService {
     async getTrendingMovies() {
         try {
             const response = await axios.get(`${BASE_URL}trending/movie/day?api_key=${KEY}&page=${this.currentPage}`);
-          
             return response.data;
         } catch (error) {
             console.error(error);
         }
     }
 
-       async searchMovies() {
+    async searchMovies() {
         try {
             const response = await axios.get(`${BASE_URL}search/movie?api_key=${KEY}&language=en-US&page=${this.currentPage}&include_adult=false&query=${this.inputValue}`);
             console.log(response.data);
-            // Збільшуємо значення this.page += 1
-            // this.incrementPage();
             return response.data;
         } catch (error) {
             console.error(error);
         }
     }
-    //  // Функція збільшення значення this.page
-    // incrementPage() {
-    //     this.page += 1;
-    // }
-    // Функція оновлення значення this.page
+  
+    // метод оновлення значення this.currentPage
     resetCurrentPage() {
         this.currentPage = 1
     }
@@ -67,4 +58,3 @@ export default class MovieApiService {
         this.currentPage = newPage
     }
 }
- 
