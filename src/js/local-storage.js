681fb38fe503ae6cb45b872457e0d7880b958a51
Andrew <shatokhin.a.p@gmail.com>
2023-02-01 09:22:43 +0200

    local-storage.rev.3

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -1,14 +1,5 @@
 import no_image from '../images/no-image.jpg';
-import { checkGenresById } from './checkGenresById';
-
-// !!!!!!!!!!добавил это  !!!!!!!!!!!!!!!!
-
-import { paginationMarkUp } from './pagination.js';
-const paginationBox = document.querySelector('.pagination');
-let currentPage = 1;
-let maxPage = 1;
-
-// -----------------------
+import { checkGenresById } from './checkGenresById.js';
 
 const watchedLibBtn = document.querySelector('.js-watched-lib-btn');
 const queueLibBtn = document.querySelector('.js-queue-lib-btn');
@@ -16,46 +7,26 @@ const libraryBox = document.querySelector('.js-library-gallery');
 const emptyGalleryBox = document.querySelector('.info-notify-box');
 const emptyGalleryText = document.querySelector('.info-notify-text');
 
-let movieArray = [];
-
-window.addEventListener('load', function () {
-  setTimeout(function () {
-    watchedLibBtn.click();
-  }, 0);
-});
-
 // Функція розпарсить об'єкти з localStorage
 function localStorageObject(key) {
   const getObjectFromLs = localStorage.getItem(key);
   const parseObjectFromLs = JSON.parse(getObjectFromLs);
-  if (!parseObjectFromLs) {
-    return;
-  } else {
-    maxPage = Math.ceil(parseObjectFromLs.length / 20);
-    paginationMarkUp(currentPage, maxPage);
-  }
+  // console.log(parseObjectFromLs);
+
   return parseObjectFromLs;
 }
 
 // ключі від localStorage
-let watchedArray = localStorageObject('WATCHED_LIST_DATA_KEY');
-let queueArray = localStorageObject('QUEUE_LIST_DATA_KEY');
+const watchedArray = localStorageObject('WATCHED_LIST_DATA_KEY');
+const queueArray = localStorageObject('QUEUE_LIST_DATA_KEY');
 
 // Рендерить розмітку у бібліотеці по дефолту
-let startPoint = (currentPage - 1) * 20;
-let stopPoint = (currentPage - 1) * 20 + 20;
-const beforeArrey = watchedArray;
-const watchedArr = beforeArrey.slice(startPoint, stopPoint);
-const beforeArreyQe = queueArray;
-const watchedArrQe = beforeArreyQe.slice(startPoint, stopPoint);
-
+renderLibrary(watchedArray);
 watchedLibBtn.classList.add('btn-is-active');
-// paginationBox.addEventListener('click', Onclick);
 
 // Функція для рендуру розмітки
-
-function renderLibrary(arrayMovies) {
-  if (!arrayMovies) {
+export function renderLibrary(arrayMovies) {
+  if (!arrayMovies || !arrayMovies.length) {
     clearMarkup();
     return emptyGalleryBox.classList.remove('is-hidden');
   }
@@ -94,89 +65,31 @@ function renderLibrary(arrayMovies) {
 
   const libraryMarkup = arrayMovies.map(markupOneCard).join('');
   libraryBox.insertAdjacentHTML('beforeend', libraryMarkup);
-
-  // !!!!!!!!!!добавил это  !!!!!!!!!!!!!!!!
-
-  // -----------------------
 }
 
 watchedLibBtn.addEventListener('click', onClickWatched);
 queueLibBtn.addEventListener('click', onClickQueue);
 
 export function onClickWatched() {
-  // !!!!!!!!!!добавил это  !!!!!!!!!!!!!!!!
-  currentPage = 1;
-  let arr = localStorage.getItem('WATCHED_LIST_DATA_KEY');
-  let parsedArr = JSON.parse(arr);
-  maxPage = Math.ceil(parsedArr.length / 20);
-  paginationMarkUp(currentPage, maxPage);
-
-  // -----------------------
-
   watchedLibBtn.classList.add('btn-is-active');
   queueLibBtn.classList.remove('btn-is-active');
   emptyGalleryBox.classList.add('is-hidden');
-  renderLibrary(watchedArr);
+
+  const getObjectFromLs = localStorage.getItem('WATCHED_LIST_DATA_KEY');
+  const parseObjectFromLs = JSON.parse(getObjectFromLs);
+  renderLibrary(parseObjectFromLs);
 }
 
 export function onClickQueue() {
-  // !!!!!!!!!!добавил это  !!!!!!!!!!!!!!!!
-  currentPage = 1;
-  let arr = localStorage.getItem('QUEUE_LIST_DATA_KEY');
-  let parsedArr = JSON.parse(arr);
-  maxPage = Math.ceil(parsedArr.length / 20);
-  paginationMarkUp(currentPage, maxPage);
-  // -----------------------
-
   emptyGalleryBox.classList.add('is-hidden');
   queueLibBtn.classList.add('btn-is-active');
   watchedLibBtn.classList.remove('btn-is-active');
-  renderLibrary(watchedArrQe);
+
+  const getObjectFromLs = localStorage.getItem('QUEUE_LIST_DATA_KEY');
+  const parseObjectFromLs = JSON.parse(getObjectFromLs);
+  renderLibrary(parseObjectFromLs);
 }
 
 function clearMarkup() {
   libraryBox.innerHTML = '';
 }
-
-// !!!!!!!!!!добавил это  !!!!!!!!!!!!!!!!
-paginationBox.addEventListener('click', OnPagClick);
-
-function OnPagClick(evt) {
-  if (evt.target.textContent == '...') {
-    return;
-  }
-  if (evt.target.nodeName !== 'P') {
-    return;
-  }
-
-  currentPage = evt.target.textContent;
-
-  if (evt.target.textContent == '>>') {
-    currentPage = Number(targetPage.textContent) + 1;
-  }
-
-  if (evt.target.textContent == '<<') {
-    currentPage = Number(targetPage.textContent) - 1;
-  }
-
-  paginationMarkUp(currentPage, maxPage);
-  reRendering(currentPage);
-
-  // И вот тут отрисовываем карточки из массива по интексам   <<<<<-----------
-
-  function reRendering(currentPage) {
-    let startPoint = (currentPage - 1) * 20;
-    let stopPoint = (currentPage - 1) * 20 + 20;
-
-    if (watchedLibBtn.classList.contains('btn-is-active')) {
-      const beforeArrey = watchedArray;
-      const watchedArr = beforeArrey.slice(startPoint, stopPoint);
-      renderLibrary(watchedArr);
-    } else {
-      const beforeArrey = queueArray;
-      const watchedArr = beforeArrey.slice(startPoint, stopPoint);
-      renderLibrary(watchedArr);
-    }
-  }
-}
-// -----------------------
